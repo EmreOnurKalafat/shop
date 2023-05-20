54a62a265d8ce153767d4e45037a19e184aa9e28
wachetr <emre1905onur@gmail.com>
2023-05-20 18:49:35 +0300

    commit 1

---
+++ pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Announcement from "../src/components/Announcement";
+import Navbar from "../src/components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
